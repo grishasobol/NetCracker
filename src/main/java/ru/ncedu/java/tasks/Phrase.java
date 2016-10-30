8dad9737e16d6073f91fa75968fe1d3c04a495bf
Gregory <grishasobol@mail.ru>
2016-10-30 22:02:09 +0300

    add Phraser

---
+++ src/main/java/ru/ncedu/java/tasks/Phrase.java
@@ -0,0 +1,13 @@
+package ru.ncedu.java.tasks;
+
+/**
+ * Created by Gregory on 10/30/2016.
+ */
+public class Phrase {
+    public static void printHello(){
+        System.out.println("Hello");
+    }
+    public static void printGoodbye(){
+        System.out.println("Goodbye");
+    }
+}
